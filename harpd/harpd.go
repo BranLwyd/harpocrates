531bef87c423e6ad8fcee449e2eae09a1cab4669
Brandon Pitman <brandon.pitman@gmail.com>
2017-01-14 18:38:52 -0800

    Update TLS configuration.
    These are the "modern" settings from
    https://blog.gopheracademy.com/advent-2016/exposing-go-on-the-internet/.

--- harpd/harpd.go
+++ harpd/harpd.go
@@ -74,12 +74,27 @@ func main() {
 		Email:      email,
 	}
 	server := &http.Server{
-		Handler: http.HandlerFunc(contentHandler),
 		TLSConfig: &tls.Config{
-			MinVersion:     tls.VersionTLS12,
-			CipherSuites:   []uint16{tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256},
+			PreferServerCipherSuites: true,
+			CurvePreferences: []tls.CurveID{
+				tls.CurveP256,
+				tls.X25519,
+			},
+			MinVersion: tls.VersionTLS12,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
 			GetCertificate: m.GetCertificate,
 		},
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      http.HandlerFunc(contentHandler),
 	}
 	log.Printf("Serving")
 	log.Fatalf("Error while serving: %v", server.ListenAndServeTLS("", ""))
