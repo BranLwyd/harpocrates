295a7e58c5dda077d7ff983f57280b6d953765cc
Brandon Pitman <brandon.pitman@gmail.com>
2017-07-05 23:14:05 -0700

    Update security headers.
    * Switch X-Frame-Options from SAMEORIGIN to DENY. I do not intend to
      ever embed in a frame.
    * Add Referrer-Policy set to no-referrer. I don't think this will ever
      link out to another site, but if it does it should not send referrer
      information to avoid leaking the site URL.

--- handler/misc.go
+++ handler/misc.go
@@ -70,9 +70,10 @@ type secureHeaderHandler struct {
 func (shh secureHeaderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 	w.Header().Set("Content-Security-Policy", "default-src 'self'")
-	w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+	w.Header().Set("X-Frame-Options", "DENY")
 	w.Header().Set("X-XSS-Protection", "1; mode=block")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Referrer-Policy", "no-referrer")
 
 	shh.h.ServeHTTP(w, r)
 }
