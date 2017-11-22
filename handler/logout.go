e17f7c5b3bb5cd0abe8711d73bf558b86b9a93f0
Brandon Pitman <brandon.pitman@gmail.com>
2017-11-21 20:52:45 -0800

    Make /logout with no session redirect to root.
    Before, visiting /logout without a session would prompt the user to log
    in, then immediately log them out, lol.

--- handler/logout.go
+++ handler/logout.go
@@ -3,24 +3,38 @@ package handler
 import (
 	"log"
 	"net/http"
+
+	"github.com/BranLwyd/harpocrates/session"
 )
 
 // logoutHandler handles requests to log out.
-type logoutHandler struct{}
-
-func newLogout() *logoutHandler {
-	return &logoutHandler{}
+type logoutHandler struct {
+	sh *session.Handler
 }
 
-func (lh logoutHandler) authPath(r *http.Request) (string, error) {
-	return authAny, nil
+func newLogout(sh *session.Handler) *logoutHandler {
+	return &logoutHandler{
+		sh: sh,
+	}
 }
 
 func (lh logoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	sess := sessionFrom(r)
-	if sess == nil {
-		log.Print("Could not get authenticated session in password handler")
+	// Try to get an existing session with the session ID from the user's
+	// cookie; if it doesn't exist, we're already done.
+	sid, err := sessionIDFromRequest(r)
+	if err != nil {
+		log.Printf("Could not get session ID: %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	sess, err := lh.sh.GetSession(sid)
+	if err != nil && err != session.ErrNoSession {
+		log.Printf("Could not get session: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	if sess == nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
